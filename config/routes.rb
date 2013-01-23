da5f5c076304cc472a641fa0d2db16f5e5aa24f7
Aleksey Miskov <alexey@deojo.com>
2013-01-23 14:55:17 +0600

    fix routes name controller + add view drom signin

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ MfpEducation::Application.routes.draw do
 
   get 'about'   => 'pages#about',    as: 'about'
   get 'contact' => 'pages#contact',  as: 'contact'
-  get 'signin' => 'sessions#signin', as: 'signin'
+  get 'signin'  => 'session#signin', as: 'signin'
   
   root to: 'maps#index'
 end
